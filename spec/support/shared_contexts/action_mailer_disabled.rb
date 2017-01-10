b635bcaff338872ac71d9ff99236c46c81c9b6fd
Seb Ashton <seb@sebashton.com>
2017-01-10 16:22:13 +0000

    Extract shared contexts into new file

---
+++ spec/support/shared_contexts/action_mailer_disabled.rb
@@ -0,0 +1,14 @@
+shared_context 'when mailer deliveries are enabled' do
+  let!(:deliver_mailers) { true }
+  it { is_expected.not_to be_empty }
+end
+
+shared_context 'when mailer deliveries are disabled' do
+  let!(:deliver_mailers) { false }
+  it { is_expected.to be_empty }
+end
+
+shared_context 'a mailer that can be disabled' do
+  context { include_context 'when mailer deliveries are enabled' }
+  context { include_context 'when mailer deliveries are disabled' }
+end
--- spec/unit/spree/order_mailer_spec.rb
+++ spec/unit/spree/order_mailer_spec.rb
@@ -1,13 +1,3 @@
-shared_context 'when mailer deliveries are enabled' do
-  let!(:deliver_mailers) { true }
-  it { is_expected.not_to be_empty }
-end
-
-shared_context 'when mailer deliveries are disabled' do
-  let!(:deliver_mailers) { false }
-  it { is_expected.to be_empty }
-end
-
 describe 'built in solidus mailers are prevented' do
   let!(:order) { create(:order_ready_to_ship) }
 
@@ -21,21 +11,18 @@ describe 'built in solidus mailers are prevented' do
   context 'SolidusCrm::PreventOrderEmails#confirm_email' do
     before { order.finalize! }
 
-    context { include_context 'when mailer deliveries are enabled' }
-    context { include_context 'when mailer deliveries are disabled' }
+    it_behaves_like 'a mailer that can be disabled'
   end
 
   context 'SolidusCrm::PreventOrderEmails#send_cancel_email' do
     before { order.cancel! }
 
-    context { include_context 'when mailer deliveries are enabled' }
-    context { include_context 'when mailer deliveries are disabled' }
+    it_behaves_like 'a mailer that can be disabled'
   end
 
   context 'SolidusCrm::PreventOrderCartonEmail#carton_email' do
     before { order.shipments.first.ship! }
 
-    context { include_context 'when mailer deliveries are enabled' }
-    context { include_context 'when mailer deliveries are disabled' }
+    it_behaves_like 'a mailer that can be disabled'
   end
 end
